1a12625144687310ef3c30323159c0e26f722c81
terwer <cbgtyw@gmail.com>
2016-01-16 17:56:14 +0800

    Add demo

--- TGFaceView/ViewController.m
+++ TGFaceView/ViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "ViewController.h"
+#import "TGFaceView/TGFaceScrollView.h"
+
+#define kMinHeight 36
+#define kMaxHeight 200
+#define kHorizontalPadding 8
+#define kVerticalPadding 5
 
 @interface ViewController ()
+
+@property (strong, nonatomic) TGFaceScrollView *faceScrollView;
 
 @end
 
@@ -17,7 +25,38 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self initView];
+    self.view.backgroundColor = [UIColor whiteColor];
+    
 }
+
+#pragma mark - init
+
+- (void)initView{
+    UIButton *showButton = [[UIButton alloc] initWithFrame:CGRectMake(20, 20, self.view.frame.size.width - 40, 30)];
+    [showButton setTitle:@"显示表情" forState:UIControlStateNormal];
+    [showButton setBackgroundColor:[UIColor grayColor]];
+    [showButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [self.view addSubview:showButton];
+    
+    
+    [self.view addSubview:self.faceScrollView];
+}
+
+#pragma mark - getter
+
+- (TGFaceScrollView*)faceScrollView{
+    if (!_faceScrollView) {
+        CGFloat faceHeight = 200;
+        _faceScrollView = [[TGFaceScrollView alloc] initWithFrame:CGRectMake( 0,self.view.frame.size.height - faceHeight - kVerticalPadding * 2 , self.view.frame.size.width, faceHeight)];
+        //高度修正
+        _faceScrollView.delegate = self;
+        _faceScrollView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin;
+    }
+    return _faceScrollView;
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
