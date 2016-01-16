1ab7c88eb070080368bcbe823667b007fe0b0394
terwer <cbgtyw@gmail.com>
2016-01-16 16:02:13 +0800

    Predefine face config

--- TGFaceVIew/TGFaceView/TGFaceView.h
+++ TGFaceVIew/TGFaceView/TGFaceView.h
@@ -8,6 +8,14 @@
 
 #import <UIKit/UIKit.h>
 
+#define kFaceViewBgColor RGBACOLOR(246,246,248,1.0)
+
+#define kFaceItemWidth 40.0
+#define kFaceItemHeight 40.0
+#define kMaxColCount 3
+#define kRowFacesCount ((int)(SCREEN_WIDTH / kFaceItemWidth))
+#define kPageFacesCount (kRowFacesCount * kMaxColCount)
+
 @interface TGFaceView : UIView
 
 @end
